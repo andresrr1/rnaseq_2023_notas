33d350b1327969db8a0d9ba8004e0ba3ae9c1f60
andresrr1 <100377641+andresrr1@users.noreply.github.com>
2023-02-02 10:03:25 -0600

    Se agrega el primer ejemplo

--- Modelos estaísticos.R
+++ Modelos estaísticos.R
@@ -7,4 +7,25 @@ colnames(mat)
 #¿Como interpretar los nombres de las columnas de mat?
 summary(lm(log(Volume) ~ log(Height) + log(Girth), data = trees))
 ##Explore Model Matrix
-##
+##Paquete de bioconductor que nos permite entender los modelos estadisticos
+##que utilizamos gracias a visualizaciones. 
+## Datos de ejemplo
+(sampleData <- data.frame(
+  genotype = rep(c("A", "B"), each = 4),
+  treatment = rep(c("ctrl", "trt"), 4)
+))
+## Creemos las imágenes usando ExploreModelMatrix
+vd <- ExploreModelMatrix::VisualizeDesign(
+  sampleData = sampleData,
+  designFormula = ~ genotype + treatment,
+  textSizeFitted = 4
+)
+
+## Veamos las imágenes
+## Usaremos shiny otra vez
+app <- ExploreModelMatrix(
+  sampleData = sampleData,
+  designFormula = ~ genotype + treatment
+)
+if (interactive()) shiny::runApp(app)
+
